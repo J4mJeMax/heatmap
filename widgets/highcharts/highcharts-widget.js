ad4622c54c751fab10568a2fbae69af17094aac2
j4mjemax@gmail.com <h20-woda>
2021-04-22 17:38:16 +0200

    Made highcharts heatmap + added new version of highcharts

--- widgets/highcharts/highcharts-widget.js
+++ widgets/highcharts/highcharts-widget.js
@@ -6,8 +6,53 @@ class CustomHighchartsWidgetCtrl {
     }
 
     $onInit() {
-        this.columnHeaders = [["Version","Executive General and Administration","Inventory Management","Manufacturing","Quality Assurance","Sales and Marketing","Research and Development"]];
-        this.rowsData = [["Actual",-214569.02804853008,-105928.24112864747,6825.64149834103,-4314.147215432423,866378.2837408222,-204590.33887112237],["Budget",-352171.14227059274,-98795.9712060611,238.1234837768164,238.1234837768164,1512416.8967751563,-202891.60255816544],["Last Year",-309116.6502055696,-124991.3211540927,7183.136080128785,-4540.101692374503,-525266.1946542066,-244072.0727683794]];
+        Highcharts.chart('highcharts',
+            {
+                chart: {
+                    type: 'heatmap',
+                    marginTop: 40,
+                    marginBottom: 80,
+                    plotBorderWidth: 1
+                },
+
+                title: {
+                    text: ''
+                },
+
+                xAxis: {
+                    categories: ["Executive General and Administration","Inventory Management","Manufacturing","Quality Assurance","Sales and Marketing","Research and Development"]
+                },
+
+                yAxis: {
+                    categories: ['Actual', 'Budget', 'Last Year'],
+                    title: null,
+                    reversed: true
+                },
+
+                colorAxis: {
+                    minColor: '#010000',
+                    maxColor: '#FF0000'
+                },
+
+                exporting: {
+                    enabled: false
+                },
+
+                legend: {
+                    enabled: false
+                },
+
+                series: [{
+                    name: ' ',
+                    borderWidth: 1,
+                    data: [[0, 0, -214569.02804853008], [0, 1, -105928.24112864747], [0, 2, 6825.64149834103], [1, 0, -4314.147215432423], [1, 1, 866378.2837408222], [1, 2, -204590.33887112237], [2, 0, -352171.14227059274], [2, 1, -98795.9712060611], [2, 2, 238.1234837768164], [3, 0, 238.1234837768164], [3, 1, 1512416.8967751563], [3, 2, -202891.60255816544], [4, 0, -309116.6502055696], [4, 1, -124991.3211540927], [4, 2, 7183.136080128785], [5, 0, -4540.101692374503], [5, 1, -525266.1946542066], [5, 2, -244072.0727683794]],
+                    dataLabels: {
+                        enabled: true,
+                        color: '#000000'
+                    }
+                }],
+
+            });
     }
 }
 
